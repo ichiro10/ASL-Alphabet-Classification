3c8ba6d3c2ad249517f23fe1c0f4e43ed26347fc
ichiro10 <ghammaz.ali1@gmail.com>
2023-11-18 01:25:10 -0600

    Initializing VGG16

---
+++ model.py
@@ -0,0 +1,111 @@
+import os
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.layers import Dense, Dropout, Flatten
+from pathlib import Path
+import numpy as np
+
+BATCH_SIZE = 64
+
+train_generator = ImageDataGenerator(rotation_range=90, 
+                                     brightness_range=[0.1, 0.7],
+                                     width_shift_range=0.5, 
+                                     height_shift_range=0.5,
+                                     horizontal_flip=True, 
+                                     vertical_flip=True,
+                                     validation_split=0.15,
+                                     preprocessing_function=preprocess_input) # VGG16 preprocessing
+
+test_generator = ImageDataGenerator(preprocessing_function=preprocess_input) # VGG16 preprocessing
+
+# Define constants for paths
+DATASET_PATH = './Dataset/image_dataset'
+TRAIN_PATH = os.path.join(DATASET_PATH, 'train')
+VALID_PATH = os.path.join(DATASET_PATH, 'valid')
+TEST_PATH = os.path.join(DATASET_PATH, 'test')
+
+
+class_subset = sorted(os.listdir(TRAIN_PATH))
+
+
+traingen = train_generator.flow_from_directory(TRAIN_PATH,
+                                               target_size=(224, 224),
+                                               class_mode='categorical',
+                                               classes=class_subset,
+                                               subset='training',
+                                               batch_size=BATCH_SIZE, 
+                                               shuffle=True,
+                                               seed=42)
+
+
+validgen = train_generator.flow_from_directory(VALID_PATH,
+                                               target_size=(224, 224),
+                                               class_mode='categorical',
+                                               classes=class_subset,
+                                               subset='validation',
+                                               batch_size=BATCH_SIZE,
+                                               shuffle=True,
+                                               seed=42)
+
+testgen = test_generator.flow_from_directory(TEST_PATH,
+                                             target_size=(224, 224),
+                                             class_mode=None,
+                                             classes=class_subset,
+                                             batch_size=1,
+                                             shuffle=False,
+                                             seed=42)
+
+
+def create_model(input_shape, n_classes, optimizer='rmsprop', fine_tune=0):
+    """
+    Compiles a model integrated with VGG16 pretrained layers
+    
+    input_shape: tuple - the shape of input images (width, height, channels)
+    n_classes: int - number of classes for the output layer
+    optimizer: string - instantiated optimizer to use for training. Defaults to 'RMSProp'
+    fine_tune: int - The number of pre-trained layers to unfreeze.
+                If set to 0, all pretrained layers will freeze during training
+    """
+    
+    # Pretrained convolutional layers are loaded using the Imagenet weights.
+    # Include_top is set to False, in order to exclude the model's fully-connected layers.
+    conv_base = VGG16(include_top=False,
+                     weights='imagenet', 
+                     input_shape=input_shape)
+    
+    # Defines how many layers to freeze during training.
+    # Layers in the convolutional base are switched from trainable to non-trainable
+    # depending on the size of the fine-tuning parameter.
+    if fine_tune > 0:
+        for layer in conv_base.layers[:-fine_tune]:
+            layer.trainable = False
+
+    else:
+        for layer in conv_base.layers:
+            layer.trainable = False
+
+    # Create a new 'top' of the model (i.e. fully-connected layers).
+    # This is 'bootstrapping' a new top_model onto the pretrained layers.
+    top_model = conv_base.output
+    top_model = Flatten(name="flatten")(top_model)
+    top_model = Dense(4096, activation='relu')(top_model)
+    top_model = Dense(1072, activation='relu')(top_model)
+    top_model = Dropout(0.2)(top_model)
+    output_layer = Dense(n_classes, activation='softmax')(top_model)
+    
+    # Group the convolutional base and new fully-connected layers into a Model object.
+    model = Model(inputs=conv_base.input, outputs=output_layer)
+
+    # Compiles the model for training.
+    model.compile(optimizer=optimizer, 
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    
+    return model
+
+
+
+        
